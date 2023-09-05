59118e41a3a287bcf0034359325ce8bc56d797e5
Maciej Mehl <106269097+mehlsson@users.noreply.github.com>
2023-09-05 02:07:18 +0200

    add mnwrapper

---
+++ mnwrapper.py
@@ -0,0 +1,13 @@
+import torch
+
+class MNSGD(torch.optim.SGD):    
+    def step(self):
+        super().step()
+        with torch.no_grad():
+            # apply the max_norm weight correction per group of parameters
+            for group in self.param_groups:
+                # rescale iff group has specified max_norm
+                if group.get('max_norm'):
+                    for tensor in group['params']:
+                        if tensor.dim() > 1:
+                            torch.renorm(tensor, p=2, dim=0, maxnorm=group['max_norm'], out=tensor)
